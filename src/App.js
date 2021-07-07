6449b9855b1b22dba2c5cbee41181ffd9f9d47e9
Deep Gandhi <deepgandhi151@gmail.com>
2021-07-07 10:12:30 +0530

    state and form inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpenses/NewExpense'
 
 function App() {
 
@@ -7,8 +8,16 @@ function App() {
     {title:'Life Insurance', amount:367.5, date: new Date(2021,5,14)}
   ]
 
+  const saveExpenseData=(enteredExpenseData)=>{
+    const expenseData = {
+      ...enteredExpenseData
+    }
+    console.log(expenseData)
+  }
+
   return (
     <div>
+      <NewExpense onAddExpense={saveExpenseData}/>
       <Expenses data = {data}/>
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import {useState} from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
 
+const names = ['hey','hi','huilo']
+
 function ExpenseItem(props){
+
+    const [title,setTitle] = useState(props.title);
+    const clicked = () => {
+        setTitle(names[Math.floor(Math.random()*3)])
+        console.log(title)
+        
+    }
     return (
         <Card className='expense-item'>
              <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        <button>Change Item</button>
+        <button onClick={clicked} >Change Item</button>
             </Card>
     )
 }
